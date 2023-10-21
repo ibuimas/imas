2e36967e639c96a6bbae1f7639cc95e96c6c759e
ibuimas <146656622+ibuimas@users.noreply.github.com>
2023-10-22 03:54:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
 module.exports = {
-  telegraf_token: "6435355248:AAHVkyipV-1v84XYJUJEhNsWBIFKTeQlGxM",
+  telegraf_token: "6431238622:AAHYMeE3Ml392h41NFO7daeJWA4a5NLrMLs",
   mongoURL: "mongodb+srv://Lucifer:Lucifer@cluster0.uh7tqik.mongodb.net/",
 };
